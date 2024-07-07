81faabcef9b5e2cb32c61673dcca12572ddc086c
syaokifaradisa09 <syaokifaradisa09@gmail.com>
2024-07-07 04:06:24 +0000

    feat: add middleware for parsing request data

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,11 @@ import express from 'express';
 
 const app = express()
 
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
 app.listen(3000, () => {
     console.log("Server started on port 3000");
 });
