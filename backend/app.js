c04eb558ead254ecc441fe927f0e8162f01b1f38
syaokifaradisa09 <syaokifaradisa09@gmail.com>
2024-07-07 02:33:26 +0000

    feat: install express js

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express()
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
+
+app.get('/', (request, response) => {
+    response.send("Hello World");
+});
