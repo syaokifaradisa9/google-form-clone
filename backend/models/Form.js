b67c24402a46f5330ba1f34c2f1586cb2eaba824
Muhammad Syaoki Faradisa <syaokifaradisa09@gmail.com>
2024-07-11 09:48:52 +0800

    feat: create Form Model

---
+++ backend/models/Form.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose"
+
+const Schema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    title: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    questions: {
+        type: Array
+    },
+    invites: {
+        type: Array
+    },
+    public: {
+        type: Boolean
+    },
+    createdAt: {
+        type: Number
+    },
+    updatedAt: {
+        type: Number
+    }
+},{
+    timestamps: {
+        currentTime: () => Math.floor(Date.now() / 1000)
+    }
+})
+
+export default mongoose.model('Form', Schema)
